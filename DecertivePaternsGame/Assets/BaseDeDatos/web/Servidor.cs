7c8d23052512a559c1da11fd995364c589b9ce13
Valentina Restrepo <valentina.re.san@gmail.com>
2024-09-08 20:00:04 -0500

    conectando con base de datos

---
+++ DecertivePaternsGame/Assets/BaseDeDatos/web/Servidor.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[CreateAssetMenu(fileName = "Servidor", menuName = "Game/Servidor", order = 1)]
+public class Servidor : ScriptableObject
+{
+    public string servidor;
+    public Servicio[] servicios;
+
+    public bool ocupado = false;
+    public Respuesta respuesta;
+    public IEnumerator ConsumirServicio(string nombre, string[] datos)
+    {
+        ocupado = true;
+        WWWForm formulario = new WWWForm();
+        Servicio s = new Servicio();
+        for (int i = 0; i < servicios.Length; i++)
+        {
+            if (servicios[i].Equals(nombre))
+            {
+                s = servicios[i];
+            }
+        }
+        for (int i = 0; i < s.parametros.Length; i++)
+        {
+            formulario.AddField(s.parametros[i], datos[i]);
+        }
+        UnityWebRequest www = UnityWebRequest.Post(servidor + "/" + s.URL, formulario);
+        Debug.Log(servidor + "/" + s.URL);
+        yield return www.SendWebRequest();
+
+        if(www.result != UnityWebRequest.Result.Success)
+        {
+            respuesta = new Respuesta();
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+            respuesta = JsonUtility.FromJson<Respuesta>(www.downloadHandler.text);
+        }
+        ocupado = false;
+    }
+}
+
+[System.Serializable]
+public class Servicio
+{
+    /*Se va a acceder a un servicio(nombre) a travez de un archivo php (URL)
+    y tiene tantos parametros*/
+    public string nombre;
+    public string URL;
+    public string[] parametros; 
+}
+
+[System.Serializable]
+public class Respuesta
+{
+    /*Se va a acceder a un servicio(nombre) a travez de un archivo php (URL)
+    y tiene tantos parametros*/
+    public int codigo;
+    public string mensaje;
+
+    public Respuesta()
+    {
+        codigo = 404;
+        mensaje = "Error";
+    }
+}
